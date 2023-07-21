35d09b3dee83ba399b400abc94b60a5347bb4058
sparkwithshubham <sparkwithshubham>
2023-07-21 13:12:02 +0000

    pyspark file

--- spark_practice 2023-07-21 10:02:53.py
+++ spark_practice 2023-07-21 10:02:53.py
@@ -104,4 +104,90 @@ df.select(df.name, *keyCols).show()
 
 # COMMAND ----------
 
+simpleData = (("James","","Smith","36636","NewYork",3100), \
+    ("Michael","Rose","","40288","California",4300), \
+    ("Robert","","Williams","42114","Florida",1400), \
+    ("Maria","Anne","Jones","39192","Florida",5500), \
+    ("Jen","Mary","Brown","34561","NewYork",3000) \
+  )
+
+# COMMAND ----------
+
+columns= ["firstname","middlename","lastname","id","location","salary"]
+
+
+# COMMAND ----------
+
+df = spark.createDataFrame(data = simpleData, schema = columns)
+
+# COMMAND ----------
+
+df.printSchema()
+df.show(truncate=False)
+
+
+# COMMAND ----------
+
+#drop column by different types of methods
+
+df.drop("firstname")
+
+
+# COMMAND ----------
+
+df.drop(col("firstname"))
+
+# COMMAND ----------
+
+df.drop(df.firstname) 
+
+# COMMAND ----------
+
+#drop all columns by creating new list of it
+
+cols = ("firstname","middlename","lastname")
+
+df.drop(*cols) \
+
+# COMMAND ----------
+
+df.na.drop().show(truncate=False)
+
+# COMMAND ----------
+
+arrayData = [
+        ('James',['Java','Scala'],{'hair':'black','eye':'brown'}),
+        ('Michael',['Spark','Java',None],{'hair':'brown','eye':None}),
+        ('Robert',['CSharp',''],{'hair':'red','eye':''}),
+        ('Washington',None,None),
+        ('Jefferson',['1','2'],{})
+        ]
+
+# COMMAND ----------
+
+df = spark.createDataFrame(data=arrayData, schema = ['name','knownLanguages','properties'])
+df.show()
+
+# COMMAND ----------
+
+df2 = df.select(df.name,explode(df.knownLanguages))
+df2.printSchema()
+df2.show()
+
+# COMMAND ----------
+
+df3 = df.select(df.name,explode(df.properties))
+df3.show()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import explode_outer,posexplode
+df.select(df.name,explode_outer(df.knownLanguages)).show()
+
+# COMMAND ----------
+
+df.select(df.name,posexplode(df.knownLanguages)).show()
+
+# COMMAND ----------
+
 
